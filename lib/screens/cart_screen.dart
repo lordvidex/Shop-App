c0a21d08c87fbafed4c0528f5bb67a7713ce060c
lordvidex <lordvidex.prime@gmail.com>
2020-01-12 23:52:45 +0300

    Bug fix 1

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -10,74 +10,88 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cart = Provider.of<Cart>(context);
-    final insideCart = cart.items.values.toList();
     return Scaffold(
       appBar: AppBar(
         title: Text('My Cart'),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            margin: EdgeInsets.all(15),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Total',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Spacer(),
-                  Chip(
-                    label: Text(
-                      '\$${cart.totalAmount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryTextTheme.title.color,
-                      ),
+      body: CartBody(),
+    );
+  }
+}
+
+class CartBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    final insideCart = cart.items.values.toList();
+    return Column(
+      children: <Widget>[
+        Card(
+          margin: EdgeInsets.all(15),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Total',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Spacer(),
+                Chip(
+                  label: Text(
+                    '\$${cart.totalAmount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryTextTheme.title.color,
                     ),
-                    backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(
-                    onPressed: () {
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    if (cart.items.values.toList().isEmpty) {
+                      Scaffold.of(context).removeCurrentSnackBar();
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Cart is Empty!'),
+                      ));
+                    } else {
                       Provider.of<Orders>(context).addOrder(
                         cartProducts: cart.items.values.toList(),
                         total: cart.totalAmount,
                       );
                       cart.clear();
-                    },
-                    child: Text(
-                      'ORDER NOW',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    }
+                  },
+                  child: Text(
+                    'ORDER NOW',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (ctx, index) => ci.CartItem(
-                id: insideCart[index].id,
-                price: insideCart[index].price,
-                quantity: insideCart[index].quantity,
-                title: insideCart[index].title,
-              ),
-              itemCount: insideCart.length,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => ci.CartItem(
+              id: insideCart[index].id,
+              price: insideCart[index].price,
+              quantity: insideCart[index].quantity,
+              title: insideCart[index].title,
             ),
+            itemCount: insideCart.length,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
